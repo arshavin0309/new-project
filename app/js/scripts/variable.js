72c402df9f400504c8cacbcb2067b27c1d1e81c4
Владислав <arshavin0309@mail.ru>
2025-04-11 14:44:46 +0500

    add

--- app/js/scripts/variable.js
+++ app/js/scripts/variable.js
@@ -2,6 +2,7 @@ if (page == '/') {
     $('.hero__heading').html('VertexTrade — your path to success on the financial markets!');
     $('.hero__title').html('Trade with VertexTrade — discover the world of financial opportunities');
     $('.hero__info').css('display', 'none');
+    $('.hero__box').css('display', 'none');
     $('.hero__btn span').html('sign up now');
 }
 
@@ -10,6 +11,7 @@ if (page == '/platform.html') {
     $('.hero__title').html('VertexTrade: your gateway to smart trading');
     $('.hero__info').html('The key to earning in the financial markets with instant server connection on any device — PC, smartphone, or tablet.');
     $('.hero__btn span').html('start trading now');
+    $('.hero__box').css('display', 'none');
     $('.terms-steps .heading .h1').html('04');
     $('.main-form .heading .h1').html('05');
 }
@@ -19,7 +21,7 @@ if (page == '/terms.html') {
     $('.hero__title').html('Invest confidently and reach your financial goals faster with VertexTrade');
     $('.hero__info').css('display', 'none');
     $('.hero__btn span').html('sign up now');
-
+    $('.hero__box').css('display', 'none');
     $('.main-plans .heading .h1').html('01');
     $('.terms-steps .heading .h1').html('02');
     $('.main-advantages .heading .h1').html('03');
@@ -31,6 +33,7 @@ if (page == '/instruments.html') {
     $('.hero__title').html('Explore VertexTrade\'s powerful trading instruments');
     $('.hero__info').css('display', 'none');
     $('.hero__btn span').html('sign up now');
+    $('.hero__box').css('display', 'none');
     $('.main-advantages .heading .h1').html('03');
     $('.main-advantages .btn span').html('start trading');
     $('.main-form .heading .h1').html('04');
@@ -41,6 +44,7 @@ if (page == '/calendar.html') {
     $('.hero__title').html('Stay ahead of the market with the Economic calendar');
     $('.hero__info').html('Keep track of key financial events that impact the markets with our real-time Economic calendar. Whether you\'re a day trader, investor, or market analyst, staying on top of upcoming economic releases and global financial reports is essential to making smart trading decisions.');
     $('.hero__btn span').html('sign up now');
+    $('.hero__box').css('display', 'none');
     $('.terms-steps .heading .h1').html('02');
     $('.main-advantages .heading .h1').html('03');
     $('.main-advantages .btn span').html('start trading');
@@ -52,7 +56,7 @@ if (page == '/404.html') {
     $('.hero__title').html('Oops! Page not found (404)');
     $('.hero__info').css('display', 'none');
     $('.hero__btn span').html('Return to the homepage');
-
+    $('.hero__box').css('display', 'none');
     $('.error .main-form .heading .h1').css('display', 'none');
     $('.error .main-form .heading h2').html('Contact our 24/7 support team if you need assistance');
 }
@@ -62,6 +66,7 @@ if (page == '/overview.html') {
     $('.hero__title').html('Market overview — stay ahead with VertexTrade');
     $('.hero__info').html('With VertexTrade, you’re always up to date with key news, trends, and events shaping the global financial markets.');
     $('.hero__btn span').html('Open an Account');
+    $('.hero__box').css('display', 'none');
     $('.main-form .heading .h1').html('05');
 }
 
@@ -70,7 +75,7 @@ if (page == '/analytics.html') {
     $('.hero__title').html('VertexTrade – your source of reliable market insights');
     $('.hero__info').html('Stay ahead of the market with VertexTrade’s objective analysis, forecasts, and key financial data. Gain the confidence to make well-informed trading decisions.');
     $('.hero__btn span').html('Start Trading');
-
+    $('.hero__box').css('display', 'none');
     $('.terms-steps .heading .h1').html('03');
     $('.main-form .heading .h1').html('04');
 }
@@ -80,6 +85,7 @@ if (page == '/news.html') {
     $('.hero__title').html('Get Real-Time Market Updates');
     $('.hero__info').html('Stay informed about the latest financial trends and breaking news to make well-founded trading decisions. Keep a competitive edge with up-to-date market insights.');
     $('.hero__btn span').html('Start Trading');
+    $('.hero__box').css('display', 'none');
 }
 
 if (page == '/forecasts.html') {
@@ -87,6 +93,7 @@ if (page == '/forecasts.html') {
     $('.hero__title').html('Find the best trading opportunities');
     $('.hero__info').html('Stay ahead of the game with regular updates on currency markets, commodities and other assets.');
     $('.hero__btn span').html('Start Trading');
+    $('.hero__box').css('display', 'none');
 }
 
 if (page == '/about.html') {
@@ -94,11 +101,16 @@ if (page == '/about.html') {
     $('.hero__title').html('Your Trusted Trading Partner');
     $('.hero__info').css('display', 'none');
     $('.hero__btn').css('display', 'none');
+    $('.hero__box').css('display', 'none');
     $('.terms-steps .heading .h1').html('04');
     $('.about .main-form .heading').css('display', 'none');
 }
 
 if (page == '/contacts.html') {
+    $('.hero__heading').html('Contact us');
+    $('.hero__title').html('Get in Touch with VertexTrade');
+    $('.hero__info').css('display', 'none');
+    $('.hero__btn').css('display', 'none');
     $('.contacts .main-form .heading .h1').html('01');
     $('.contacts .main-form .heading h2').html('Feel free to message us anytime');
 }
