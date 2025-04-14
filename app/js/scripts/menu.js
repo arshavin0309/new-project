34ea6bf3c0230056a8986b83a21d1a4ef33c15c6
Владислав <arshavin0309@mail.ru>
2025-04-14 14:52:57 +0500

    add

--- app/js/scripts/menu.js
+++ app/js/scripts/menu.js
@@ -1,26 +1,30 @@
 // мобильное меню
 
+let header = $('.header');
 let menuItem = $('.header .menu > .menu-item');
 let subMenu = $('.header .menu > .menu-item .sub-menu');
 let burger = $('.header__burger'); // кнопка открытия мобильного меню
 let headerMenu = $('.header .menu'); // меню хедера
-let headerBox = $('.header'); // блок внутри контейнера хедера, например если он в виде острова и при выпадении мобильного меню, нужно его дополнительно стилизовать
+let headerNav = $('.header__nav'); // блок внутри контейнера хедера, например если он в виде острова и при выпадении мобильного меню, нужно его дополнительно стилизовать
+let mobileWidth = 1200; // ширина, с которой начинается мобильная версия хедера
 
+header.removeClass('active');
 menuItem.removeClass('active');
 subMenu.removeClass('active').slideUp();
 burger.removeClass('active');
 headerMenu.removeClass('active');
-headerBox.removeClass('active');
+headerNav.removeClass('active');
 
 navMenu();
 
 function navMenu() {
-    if ($(window).width() <= 1200) {
+    if ($(window).width() <= mobileWidth) {
 
         burger.on('click', function () {
             console.log('click on burger');
             burger.toggleClass('active');
-            headerBox.toggleClass('active');
+            headerNav.toggleClass('active');
+            header.toggleClass('active');
             headerMenu.toggleClass('active');
 
             subMenu.removeClass('active').slideUp();
@@ -29,7 +33,8 @@ function navMenu() {
 
         $('.upButton').on('click', function () {
             burger.removeClass('active');
-            headerBox.removeClass('active');
+            headerNav.removeClass('active');
+            header.removeClass('active');
             headerMenu.removeClass('active');
 
             subMenu.removeClass('active').slideUp();
@@ -38,33 +43,53 @@ function navMenu() {
 
         for (let click = 0; click < menuItem.length; click++) {
             menuItem.eq(click).on('click', function () {
-                if (menuItem.eq(click).hasClass('active')) {
-                    menuItem.eq(click).removeClass('active');
-                    subMenu.eq(click).removeClass('active').slideUp();
-                } else {
-                    for (let other = 0; other < menuItem.length; other++) {
-                        if (menuItem.eq(other) != menuItem.eq(click)) {
-                            subMenu.eq(other).removeClass('active').slideUp();
-                            menuItem.removeClass('active');
+                if ($(window).width() <= mobileWidth) {
+                    if (menuItem.eq(click).hasClass('active')) {
+                        menuItem.eq(click).removeClass('active');
+                        subMenu.eq(click).removeClass('active').slideUp();
+                    } else {
+                        for (let other = 0; other < menuItem.length; other++) {
+                            if (menuItem.eq(other) != menuItem.eq(click)) {
+                                subMenu.eq(other).removeClass('active').slideUp();
+                                menuItem.removeClass('active');
+                            }
                         }
-                    }
 
-                    subMenu.eq(click).addClass('active').slideDown();
-                    menuItem.eq(click).addClass('active');
+                        subMenu.eq(click).addClass('active').slideDown();
+                        menuItem.eq(click).addClass('active');
+                    }
                 }
             })
         }
     } else {
         for (let hover = 0; hover < menuItem.length; hover++) {
-            menuItem.eq(hover).hover(
-                function () {
-                    menuItem.eq(hover).addClass('active');
-                    subMenu.eq(hover).slideDown();
-                },
-                function () {
-                    subMenu.eq(hover).slideUp();
-                }
-            )
+            if ($(window).width() >= mobileWidth) {
+                menuItem.eq(hover).hover(
+                    function () {
+                        menuItem.eq(hover).addClass('active');
+                        subMenu.eq(hover).slideDown();
+
+                        if (hover == 0) {
+                            headerMenu.addClass('first');
+                        }
+
+                        if (hover == menuItem.length - 1) {
+                            headerMenu.addClass('last');
+                        }
+                    },
+                    function () {
+                        subMenu.eq(hover).slideUp();
+
+                        if (hover == 0) {
+                            headerMenu.removeClass('first');
+                        }
+
+                        if (hover == menuItem.length - 1) {
+                            headerMenu.removeClass('last');
+                        }
+                    }
+                )
+            }
         }
     }
 }
--- app/js/scripts/variable.js
+++ app/js/scripts/variable.js
@@ -2,18 +2,19 @@ if (page == '/') {
     $('.hero__heading').html('VertexTrade — your path to success on the financial markets!');
     $('.hero__title').html('Trade with VertexTrade — discover the world of financial opportunities');
     $('.hero__info').css('display', 'none');
-    $('.hero__box').css('display', 'none');
     $('.hero__btn span').html('sign up now');
 }
 
 if (page == '/platform.html') {
     $('.hero__heading').html('Trading Platform');
     $('.hero__title').html('VertexTrade: your gateway to smart trading');
-    $('.hero__info').html('The key to earning in the financial markets with instant server connection on any device — PC, smartphone, or tablet.');
+    $('.hero__info').html('The key to earning in the financial markets with instant server connection on any device — Desktop, smartphone, or tablet.');
     $('.hero__btn span').html('start trading now');
-    $('.hero__box').css('display', 'none');
-    $('.terms-steps .heading .h1').html('04');
+    $('.main-steps .heading .h1').html('04');
     $('.main-form .heading .h1').html('05');
+    $('.banner-1 h2').html('Unlock your potential with every trade');
+    $('.banner-1 h2 + p').html('Join us and start trading today!');
+    $('.banner-1 .btn span').html('open an account');
 }
 
 if (page == '/terms.html') {
@@ -21,11 +22,13 @@ if (page == '/terms.html') {
     $('.hero__title').html('Invest confidently and reach your financial goals faster with VertexTrade');
     $('.hero__info').css('display', 'none');
     $('.hero__btn span').html('sign up now');
-    $('.hero__box').css('display', 'none');
     $('.main-plans .heading .h1').html('01');
-    $('.terms-steps .heading .h1').html('02');
+    $('.main-steps .heading .h1').html('02');
     $('.main-advantages .heading .h1').html('03');
     $('.main-form .heading .h1').html('04');
+    $('.banner-1 h2').html('Trade with VertexTrade and achieve financial success in the global markets!');
+    $('.banner-1 h2 + p').html('Join us and start trading today!');
+    $('.banner-1 .btn span').html(' Start trading');
 }
 
 if (page == '/instruments.html') {
@@ -33,10 +36,12 @@ if (page == '/instruments.html') {
     $('.hero__title').html('Explore VertexTrade\'s powerful trading instruments');
     $('.hero__info').css('display', 'none');
     $('.hero__btn span').html('sign up now');
-    $('.hero__box').css('display', 'none');
     $('.main-advantages .heading .h1').html('03');
     $('.main-advantages .btn span').html('start trading');
     $('.main-form .heading .h1').html('04');
+    $('.banner-1 h2').html('Trade with VertexTrade and achieve financial success in the global markets!');
+    $('.banner-1 h2 + p').html('Join us and start trading today!');
+    $('.banner-1 .btn span').html('Start trading');
 }
 
 if (page == '/calendar.html') {
@@ -44,8 +49,7 @@ if (page == '/calendar.html') {
     $('.hero__title').html('Stay ahead of the market with the Economic calendar');
     $('.hero__info').html('Keep track of key financial events that impact the markets with our real-time Economic calendar. Whether you\'re a day trader, investor, or market analyst, staying on top of upcoming economic releases and global financial reports is essential to making smart trading decisions.');
     $('.hero__btn span').html('sign up now');
-    $('.hero__box').css('display', 'none');
-    $('.terms-steps .heading .h1').html('02');
+    $('.main-steps .heading .h1').html('02');
     $('.main-advantages .heading .h1').html('03');
     $('.main-advantages .btn span').html('start trading');
     $('.main-form .heading .h1').html('04');
@@ -56,7 +60,6 @@ if (page == '/404.html') {
     $('.hero__title').html('Oops! Page not found (404)');
     $('.hero__info').css('display', 'none');
     $('.hero__btn span').html('Return to the homepage');
-    $('.hero__box').css('display', 'none');
     $('.error .main-form .heading .h1').css('display', 'none');
     $('.error .main-form .heading h2').html('Contact our 24/7 support team if you need assistance');
 }
@@ -66,8 +69,10 @@ if (page == '/overview.html') {
     $('.hero__title').html('Market overview — stay ahead with VertexTrade');
     $('.hero__info').html('With VertexTrade, you’re always up to date with key news, trends, and events shaping the global financial markets.');
     $('.hero__btn span').html('Open an Account');
-    $('.hero__box').css('display', 'none');
     $('.main-form .heading .h1').html('05');
+    $('.banner-1 h2').html('Your Trusted<br>Trading Partner');
+    $('.banner-1 h2 + p').html('Trading with VertexTrade means choosing a reliable partner for financial success in the global markets.');
+    $('.banner-1 .btn span').html('open an account');
 }
 
 if (page == '/analytics.html') {
@@ -75,9 +80,11 @@ if (page == '/analytics.html') {
     $('.hero__title').html('VertexTrade – your source of reliable market insights');
     $('.hero__info').html('Stay ahead of the market with VertexTrade’s objective analysis, forecasts, and key financial data. Gain the confidence to make well-informed trading decisions.');
     $('.hero__btn span').html('Start Trading');
-    $('.hero__box').css('display', 'none');
-    $('.terms-steps .heading .h1').html('03');
+    $('.main-steps .heading .h1').html('03');
     $('.main-form .heading .h1').html('04');
+    $('.banner-1 h2').html('Trade with Confidence Using Advanced Analytics');
+    $('.banner-1 h2 + p').html('VertexTrade – your analytics, your strategy, your success. Start trading with confidence today!');
+    $('.banner-1 .btn span').html('start trading');
 }
 
 if (page == '/news.html') {
@@ -85,7 +92,6 @@ if (page == '/news.html') {
     $('.hero__title').html('Get Real-Time Market Updates');
     $('.hero__info').html('Stay informed about the latest financial trends and breaking news to make well-founded trading decisions. Keep a competitive edge with up-to-date market insights.');
     $('.hero__btn span').html('Start Trading');
-    $('.hero__box').css('display', 'none');
     $('.banner-1 h2').html('reach your maximum potential with vertextrade');
     $('.banner-1 h2 + p').html('VertexTrade – your analytics, your strategy, your success. Start trading with confidence today!');
 
@@ -96,7 +102,6 @@ if (page == '/forecasts.html') {
     $('.hero__title').html('Find the best trading opportunities');
     $('.hero__info').html('Stay ahead of the game with regular updates on currency markets, commodities and other assets.');
     $('.hero__btn span').html('Start Trading');
-    $('.hero__box').css('display', 'none');
     $('.banner-1 h2').html('Trade with Confidence, Trade with VertexTrade');
     $('.banner-1 h2 + p').html('Your strategy, your success—gain the insights you need to stay ahead!');
 }
@@ -106,8 +111,7 @@ if (page == '/about.html') {
     $('.hero__title').html('Your Trusted Trading Partner');
     $('.hero__info').css('display', 'none');
     $('.hero__btn').css('display', 'none');
-    $('.hero__box').css('display', 'none');
-    $('.terms-steps .heading .h1').html('04');
+    $('.main-steps .heading .h1').html('04');
     $('.about .main-form .heading').css('display', 'none');
     $('.banner-1 h2').html('Ready to trade?<br>Click below to jump back into the action!');
     $('.banner-1 h2 + p').css('display', 'none');
@@ -119,6 +123,7 @@ if (page == '/contacts.html') {
     $('.hero__title').html('Get in Touch with VertexTrade');
     $('.hero__info').css('display', 'none');
     $('.hero__btn').css('display', 'none');
+    $('.hero__box').css('display', 'block');
     $('.contacts .main-form .heading .h1').html('01');
     $('.contacts .main-form .heading h2').html('Feel free to message us anytime');
     $('.banner-1 h2').html('Ready to trade?<br>Click below to jump back into the action!');
