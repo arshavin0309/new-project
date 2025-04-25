234b6e934d27e72a3f67a5646568aad668de598a
Владислав <arshavin0309@mail.ru>
2025-04-25 14:56:32 +0500

    add

--- app/js/scripts/variable.js
+++ app/js/scripts/variable.js
@@ -1,13 +1,13 @@
 if (page == '/') {
-    $('.hero__heading').html('VertexTrade — your path to success on the financial markets!');
-    $('.hero__title').html('Trade with VertexTrade — discover the world of financial opportunities');
+    $('.hero__heading').html('NewProject — your path to success on the financial markets!');
+    $('.hero__title').html('Trade with NewProject — discover the world of financial opportunities');
     $('.hero__info').css('display', 'none');
     $('.hero__btn span').html('sign up now');
 }
 
 if (page == '/platform.html') {
     $('.hero__heading').html('Trading Platform');
-    $('.hero__title').html('VertexTrade: your gateway to smart trading');
+    $('.hero__title').html('NewProject: your gateway to smart trading');
     $('.hero__info').html('The key to earning in the financial markets with instant server connection on any device — Desktop, smartphone, or tablet.');
     $('.hero__btn span').html('start trading now');
     $('.main-steps .heading .h1').html('04');
@@ -19,27 +19,27 @@ if (page == '/platform.html') {
 
 if (page == '/terms.html') {
     $('.hero__heading').html('Trading terms');
-    $('.hero__title').html('Invest confidently and reach your financial goals faster with VertexTrade');
+    $('.hero__title').html('Invest confidently and reach your financial goals faster with NewProject');
     $('.hero__info').css('display', 'none');
     $('.hero__btn span').html('sign up now');
     $('.main-plans .heading .h1').html('01');
     $('.main-steps .heading .h1').html('02');
     $('.main-advantages .heading .h1').html('03');
     $('.main-form .heading .h1').html('04');
-    $('.banner-1 h2').html('Trade with VertexTrade and achieve financial success in the global markets!');
+    $('.banner-1 h2').html('Trade with NewProject and achieve financial success in the global markets!');
     $('.banner-1 h2 + p').html('Join us and start trading today!');
     $('.banner-1 .btn span').html(' Start trading');
 }
 
 if (page == '/instruments.html') {
     $('.hero__heading').html('Trading instruments');
-    $('.hero__title').html('Explore VertexTrade\'s powerful trading instruments');
+    $('.hero__title').html('Explore NewProject\'s powerful trading instruments');
     $('.hero__info').css('display', 'none');
     $('.hero__btn span').html('sign up now');
     $('.main-advantages .heading .h1').html('03');
     $('.main-advantages .btn span').html('start trading');
     $('.main-form .heading .h1').html('04');
-    $('.banner-1 h2').html('Trade with VertexTrade and achieve financial success in the global markets!');
+    $('.banner-1 h2').html('Trade with NewProject and achieve financial success in the global markets!');
     $('.banner-1 h2 + p').html('Join us and start trading today!');
     $('.banner-1 .btn span').html('Start trading');
 }
@@ -67,24 +67,24 @@ if (page == '/404.html') {
 
 if (page == '/overview.html') {
     $('.hero__heading').html('Market Trends');
-    $('.hero__title').html('Market overview — stay ahead with VertexTrade');
-    $('.hero__info').html('With VertexTrade, you’re always up to date with key news, trends, and events shaping the global financial markets.');
+    $('.hero__title').html('Market overview — stay ahead with NewProject');
+    $('.hero__info').html('With NewProject, you’re always up to date with key news, trends, and events shaping the global financial markets.');
     $('.hero__btn span').html('Open an Account');
     $('.main-form .heading .h1').html('05');
     $('.banner-1 h2').html('Your Trusted<br>Trading Partner');
-    $('.banner-1 h2 + p').html('Trading with VertexTrade means choosing a reliable partner for financial success in the global markets.');
+    $('.banner-1 h2 + p').html('Trading with NewProject means choosing a reliable partner for financial success in the global markets.');
     $('.banner-1 .btn span').html('open an account');
 }
 
 if (page == '/analytics.html') {
     $('.hero__heading').html('Market analytics');
-    $('.hero__title').html('VertexTrade – your source of reliable market insights');
-    $('.hero__info').html('Stay ahead of the market with VertexTrade’s objective analysis, forecasts, and key financial data. Gain the confidence to make well-informed trading decisions.');
+    $('.hero__title').html('NewProject – your source of reliable market insights');
+    $('.hero__info').html('Stay ahead of the market with NewProject’s objective analysis, forecasts, and key financial data. Gain the confidence to make well-informed trading decisions.');
     $('.hero__btn span').html('Start Trading');
     $('.main-steps .heading .h1').html('03');
     $('.main-form .heading .h1').html('04');
     $('.banner-1 h2').html('Trade with Confidence Using Advanced Analytics');
-    $('.banner-1 h2 + p').html('VertexTrade – your analytics, your strategy, your success. Start trading with confidence today!');
+    $('.banner-1 h2 + p').html('NewProject – your analytics, your strategy, your success. Start trading with confidence today!');
     $('.banner-1 .btn span').html('start trading');
 }
 
@@ -93,8 +93,8 @@ if (page == '/news.html') {
     $('.hero__title').html('Get Real-Time Market Updates');
     $('.hero__info').html('Stay informed about the latest financial trends and breaking news to make well-founded trading decisions. Keep a competitive edge with up-to-date market insights.');
     $('.hero__btn span').html('Start Trading');
-    $('.banner-1 h2').html('reach your maximum potential with vertextrade');
-    $('.banner-1 h2 + p').html('VertexTrade – your analytics, your strategy, your success. Start trading with confidence today!');
+    $('.banner-1 h2').html('reach your maximum potential with NewProject');
+    $('.banner-1 h2 + p').html('NewProject – your analytics, your strategy, your success. Start trading with confidence today!');
 
 }
 
@@ -103,12 +103,12 @@ if (page == '/forecasts.html') {
     $('.hero__title').html('Find the best trading opportunities');
     $('.hero__info').html('Stay ahead of the game with regular updates on currency markets, commodities and other assets.');
     $('.hero__btn span').html('Start Trading');
-    $('.banner-1 h2').html('Trade with Confidence, Trade with VertexTrade');
+    $('.banner-1 h2').html('Trade with Confidence, Trade with NewProject');
     $('.banner-1 h2 + p').html('Your strategy, your success—gain the insights you need to stay ahead!');
 }
 
 if (page == '/about.html') {
-    $('.hero__heading').html('About VertexTrade');
+    $('.hero__heading').html('About NewProject');
     $('.hero__title').html('Your Trusted Trading Partner');
     $('.hero__info').css('display', 'none');
     $('.hero__btn').css('display', 'none');
@@ -121,7 +121,7 @@ if (page == '/about.html') {
 
 if (page == '/contacts.html') {
     $('.hero__heading').html('Contact us');
-    $('.hero__title').html('Get in Touch with VertexTrade');
+    $('.hero__title').html('Get in touch with NewProject');
     $('.hero__info').css('display', 'none');
     $('.hero__btn').css('display', 'none');
     $('.hero__box').css('display', 'block');
